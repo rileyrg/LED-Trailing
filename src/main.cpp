540ca366bfcf1e7020a88a9a84a960565017e56a
Richard Riley <rileyrg@gmail.com>
2019-09-11 13:44:20 +0200

    indent, slow

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 #include <Arduino.h>
 
+#define DELAY 200
 int BASE = 3;
 int NUM = 5;
 void setup()
 {
-for (int i = BASE; i < BASE + NUM; i ++)
-{
-pinMode(i, OUTPUT);
-//set port ‘i’ as an output port
-}
+  for (int i = BASE; i < BASE + NUM; i ++)
+    {
+      pinMode(i, OUTPUT);
+      //set port ‘i’ as an output port
+    }
 }
 void loop()
 {
-for (int i = BASE; i < BASE + NUM; i ++)
-{
-digitalWrite(i, LOW);
- delay(20);
-}
-for (int i = BASE; i < BASE + NUM; i ++)
-{
-digitalWrite(i, HIGH);
- delay(20);
-}
+  for (int i = BASE; i < BASE + NUM; i ++)
+    {
+      digitalWrite(i, LOW);
+      delay(DELAY);
+    }
+  for (int i = BASE; i < BASE + NUM; i ++)
+    {
+      digitalWrite(i, HIGH);
+      delay(DELAY);
+    }
 }
